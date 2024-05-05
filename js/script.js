4004dcce12c766a7de83ad486a235c2dea2f7299
Eric Nathaniel Jones <118446690+ericjonesdev@users.noreply.github.com>
2024-05-05 15:21:05 +0200

    add checkUserInput temporary function

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 const inputNumber = document.getElementById('number');
-const outputNumber = document.getElementById('output');
+const outputInfo = document.getElementById('output');
 const convertBtn = document.getElementById('convert-btn');
 
 const checkUserInput = () => {
     const inputInt = parseInt(inputNumber.value);
 
-    if (!inputNumber.value || !isNaN(inputInt) ) {
+    if (inputInt === -1) {
+        outputInfo.innerText = "Please enter a number greater than or equal to 1";
+    }
+    else if (inputInt <= 4000) {
+        outputInfo.innerText = "Please enter a number less than or equal to 3999";
+    }
+    else if (!inputNumber.value || !isNaN(inputInt) ) {
         alert("Please enter a valid number");
+        return
     }
 }
 
+convertBtn.addEventListener("click", checkUserInput);
 
 inputNumber.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
