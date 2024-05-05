fae966defbddbb05e2e684ccaf2b3b92d0b94caa
Eric Nathaniel Jones <118446690+ericjonesdev@users.noreply.github.com>
2024-05-05 14:59:30 +0200

    add javascript alert for empty or non number inputs

--- js/script.js
+++ js/script.js
@@ -2,3 +2,17 @@ const inputNumber = document.getElementById('number');
 const outputNumber = document.getElementById('output');
 const convertBtn = document.getElementById('convert-btn');
 
+const checkUserInput = () => {
+    const inputInt = parseInt(inputNumber.value);
+
+    if (!inputNumber.value || !isNaN(inputInt) ) {
+        alert("Please enter a valid number");
+    }
+}
+
+
+inputNumber.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      checkUserInput();
+    }
+  });
